e2bdad03a236088abfaf0611833fd1f77bd23c22
deepakkumar1722 <deepak.kumar@upkarak.com>
2022-03-05 18:01:05 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import './App.css';
+import Categories from './components/Categories/';
+import { useEffect, useState } from 'react';
+import { getJoke } from './services'
+
+function App() {
+  const [selectedCategory, setSelectedCategory] = useState('');
+  const [joke, setJoke] = useState('');
+
+  const onCategorySelected = async (category) => {
+    const res = await getJoke(category);
+    setJoke(res);
+  };
+
+  return (
+    <div className="App">
+      <h3>Chuck Norris</h3>
+      <Categories
+        selectedCategory={selectedCategory}
+        setSelectedCategory={setSelectedCategory}
+        onCategorySelected={onCategorySelected}
+      />
+      {joke && (
+        <section className="joke-section">
+          <p className="joke-header">Selected Category : {selectedCategory}</p>
+          <p className="joke">{joke}</p>
+          <button
+            onClick={() => onCategorySelected(selectedCategory)}
+            className="btn"
+          >
+            New Joke
+          </button>
+        </section>
+      )}
+    </div>
+  );
+}
+
+export default App;
